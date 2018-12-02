7ad01d8f1756e9c2e0ec717bac9ff0b5b3d62784
Zack Frost <zfzackfrostdevel@gmail.com>
2018-12-02 13:36:23 -0500

    Documentation comments for `presets.ts`

--- src/presets.ts
+++ src/presets.ts
@@ -1,6 +1,18 @@
+import { WorkspaceConfiguration } from "vscode";
 
-import { WorkspaceConfiguration } from 'vscode';
-
+/**
+ * The format for user configuration of the presets for "Quick" mode
+ *
+ * @export
+ * @interface QuickPreset
+ * @property {string} label - The name of the preset to display in the selection menu
+ * @property {string} fillChar - The fill character to use when this preset style is selected
+ * @property {number} width - The width of comment bars created using this preset style
+ * @property {number} thickness - The number of lines tall the comment bar will be, when
+ * using this preset style. This value will be rounded up to the nearest odd
+ * number in order to keep the text centered vertically.
+ * @property {boolean} [seamlessFill=false] - Enable seamless filling? 
+ */
 export interface QuickPreset {
 	label: string;
 	fillChar: string;
@@ -9,12 +21,24 @@ export interface QuickPreset {
 	seamlessFill?: boolean;
 }
 
-export function getCommentBarPresets(config: WorkspaceConfiguration): QuickPreset[] {
-	if (config.has("quickPresets")) {
-		let cfg: QuickPreset[] | undefined = config.get("quickPresets");
-		if (cfg) {
-			return cfg;
-		}
+/**
+ * Get the relevant user configuration of the presets for "Quick" mode
+ *
+ * @export
+ * @param {WorkspaceConfiguration} config
+ * @returns {QuickPreset[]}
+ */
+export function getCommentBarPresets(
+	config: WorkspaceConfiguration
+): QuickPreset[] {
+	// Get value of user configuration for 	quick presets
+	let cfg: QuickPreset[] | undefined = config.get("quickPresets");
+
+	// If quick preset configuration is valid...
+	if (typeof cfg !== "undefined") {
+		// Return quick preset configuration...
+		return cfg;
 	}
+	// Else, return empty array as default
 	return [];
 }
